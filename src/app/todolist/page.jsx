dd612525bf8cf1d0e40c048c95f55d6479e32670
Lavish870 <lovishsharma870@gmail.com>
2026-08-30 13:31:11 +0530

    todo

--- src/app/todolist/page.jsx
+++ src/app/todolist/page.jsx
@@ -1,21 +1,36 @@
 'use client';
+import { Trash2 } from 'lucide-react';
 import React, { useState } from 'react'
 
 const TodoList = () => {
 
-    const [taskList, setTaskList] = useState([
-        {text : 'doodh lao', completed: false},
-        {text : 'dhaniya lao', completed: false},
-        {text : 'kapde dho', completed: false},
-    ]);
+    const [taskList, setTaskList] = useState([]);
 
     const addNewTask = (e) => {
+            
         if(e.code === 'Enter'){
-            console.log(e.target.value);
+            if(!e.target.value) {
+                alert('Task cannot be empty');
+                return;
+            }
+        
+            // console.log(e.target.value);
+
+            const newTask = { text : e.target.value, completed: false};
+
+            setTaskList([newTask, ...taskList]);
 
             e.target.value = '';
         }
+        
     };
+
+    const deleteTask = (index) => {
+
+        const temp = taskList;
+        temp.splice(index, 1);
+        setTaskList([...temp]);
+    }
 
 
   return (
@@ -37,9 +52,26 @@ const TodoList = () => {
                         <p className='font-bold text-gray-400 text-center'>No Task Here😊</p>
                     ): (
 
-                        taskList.map((task) => {
-                            return <div>
-                                <p>{task.text}</p>
+                        taskList.map((task, index) => {
+                            return <div key={index} className='mb-4 p-4 shadow-lg flex justify-between items-center'>
+                                <div className='flex gap-3'>
+                                    <input onChange={(e) => {
+                                        const temp = taskList;
+                                        taskList[index].completed = e.target.checked;
+                                        setTaskList([...temp]);
+                                    }} type="checkbox" />
+                                    <p>{task.text}</p>
+                                </div>
+
+                                {
+                                    task.completed ? (
+                                        <p className='bg-green-200 rounded-full px-3 text-green-700'>Completed</p>
+                                    ) : (
+                                        <p className='bg-yellow-200 rounded-full px-3 text-yellow-700'>Pending</p>
+                                    )
+                                }
+                                <button onClick={() => {deleteTask(index)}} className='bg-red-500 p-2 text-white rounded'>
+                                <Trash2 /></button>
                             </div>
                         })
                     )
